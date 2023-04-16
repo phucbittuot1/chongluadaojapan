fa790649cfe79224501fc8e7a6ed2170cadfc111
phucbittuot1 <130816232+phucbittuot1@users.noreply.github.com>
2023-04-16 15:06:51 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
-const search = () => {
-  const query = document.getElementById("search-input").value.trim();
-  if (query === "") {
-    document.getElementById("result").innerHTML = "Vui lòng nhập tên tài khoản hoặc số tài khoản để kiểm tra!";
-    return;
-  }
-  const xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://cors-anywhere.herokuapp.com/https://api.jsonbin.io/v3/b/643a1b26ace6f33a220ba07b/latest", true);
-  xhr.setRequestHeader("X-Master-Key", "$2b$10$1/Xt/xV7B0MueH2HdEbqAekImCKdJxOSnLRiC.c0qR4fep7Qe8Nu6");
-  xhr.setRequestHeader("X-Api-Key", "$2b$10$dkhK8Q8mhMvf61LwaqGrx.upnYnRJSVrc3pTj4Gd74lt5oLbLd8.W");
-  xhr.onload = () => {
-    if (xhr.status === 200) {
-      const data = JSON.parse(xhr.responseText);
-      const results = data.record.filter((item) => item.ten_tai_khoan === query || item.so_tai_khoan === query);
-      if (results.length > 0) {
-        let resultHtml = "";
-        results.forEach((result) => {
-          resultHtml += `Tài khoản <b>${result.ten_tai_khoan}</b> với số tài khoản <b>${result.so_tai_khoan}</b> đã bị đưa vào danh sách đen vì lừa đảo!<br>`;
-        });
-        document.getElementById("result").innerHTML = resultHtml;
-      } else {
-        document.getElementById("result").innerHTML = `Không tìm thấy thông tin tài khoản <b>${query}</b> trong danh sách đen.`;
+function searchBank(event) {
+  event.preventDefault();
+
+  var accountNumber = document.getElementById("account").value;
+  var accountName = document.getElementById("name").value;
+  
+  // sử dụng đường dẫn tương đối
+  var blacklistFile = "./blacklist.json";
+
+  fetch(blacklistFile)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      var isScam = false;
+      for (var i = 0; i < data.blacklist.length; i++) {
+        if (data.blacklist[i].so_tai_khoan === accountNumber && data.blacklist[i].ten_tai_khoan === accountName) {
+          isScam = true;
+          break;
+        }
       }
-    } else {
-      document.getElementById("result").innerHTML = "Lỗi kết nối đến server. Vui lòng thử lại sau.";
-    }
-  };
-  xhr.onerror = () => {
-    document.getElementById("result").innerHTML = "Lỗi kết nối đến server. Vui lòng thử lại sau.";
-  };
-  xhr.send();
-};
+      showResult(isScam);
+    })
+    .catch(function(error) {
+      console.error('Fetch error:', error);
+      showError();
+    });
+}
+
+function showResult(isScam) {
+  var resultElement = document.getElementById("result");
+  var notificationElement = document.getElementById("notification");
+  
+  if (isScam) {
+    resultElement.style.backgroundImage = "url('./scam.jpg')";
+    notificationElement.innerHTML = "Tài khoản này là tài khoản lừa đảo";
+  } else {
+    resultElement.style.backgroundImage = "url('./not_scam.jpg')";
+    notificationElement.innerHTML = "Tài khoản này không phải là tài khoản lừa đảo";
+  }
+}
+
+function showError() {
+  var notificationElement = document.getElementById("notification");
+  notificationElement.innerHTML = "Có lỗi xảy ra khi tải danh sách tài khoản lừa đảo";
+}
